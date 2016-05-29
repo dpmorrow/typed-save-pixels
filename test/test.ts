92e81a3f8004e60bc624392010d4d5f7a5c368ab
Douglas Morrow <douglas.p.morrow@gmail.com>
2016-05-28 19:51:09 -0700

    Add some tests

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,19 @@
 import test = require('blue-tape');
-
+import zeros = require('zeros');
 import savePixels = require('save-pixels');
+
+test('module can load properly', null, function(t) {
+  let imgsrc = zeros([1, 1, 3]);
+  let test = savePixels(imgsrc, 'png');
+
+  t.notEqual(null, test, 'created instance is not null');
+  t.end();
+});
+
+test('module can create image object', null, function(t) {
+  let imgsrc = zeros([1, 1, 3]);
+  let test = savePixels(imgsrc, 'png');
+
+  t.notEqual(undefined, test.data, 'creates a png');
+  t.end();
+});
